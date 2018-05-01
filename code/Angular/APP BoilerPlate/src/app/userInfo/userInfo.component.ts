0c6403e56c4ada89bb421717f25a088a8e3c0a86
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-05-01 17:13:36 +0100

    team, userinfo, competition

---
+++ code/Angular/APP BoilerPlate/src/app/userInfo/userInfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info',
+  templateUrl: './userInfo.component.html',
+  styleUrls: ['./userInfo.component.css']
+})
+export class UserInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
