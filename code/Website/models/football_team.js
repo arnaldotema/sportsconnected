32af3d7765cd474ab69c75a173645fda7950ef1c
unknown <dc.pires@campus.fct.unl.pt>
2018-05-10 08:07:55 +0100

    Diogo's first push <3

--- code/Website/models/football_team.js
+++ code/Website/models/football_team.js
@@ -8,7 +8,12 @@ var FootballTeamSchema = new Schema({
         default: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg'
     },
     name: { type: String, required: true, unique: true},
-    full_name: { type: String, unique: true}
+    full_name: { type: String, unique: true},
+    external_ids: {
+        zerozero: String,
+    }
 });
 
-module.exports = mongoose.model('Team', FootballTeamSchema);
+FootballTeamSchema.statics = require('../services/football_team_service');
+
+module.exports = mongoose.model('football_team', FootballTeamSchema);
