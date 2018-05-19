c3f007e3acba7d3f4cde72bf56e55faee90e2dbd
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-05-19 01:29:51 +0100

    profile news
    Former-commit-id: 36c948bc558a9b851775544f08f9b727d6efd6ee
    Former-commit-id: a1bd42a7751de8834df3d0dcaa72c3b889d2b8fc

--- code/Angular/APP BoilerPlate/src/app/app-routing.module.ts
+++ code/Angular/APP BoilerPlate/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard} from './_guards/auth.guard';
 import { User_infoComponent } from "./user_info/user_info.component";
 import {TeamComponent} from './team/team.component';
+import {User_infoMediaComponent} from './user-info-media/user-info-media.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/userInfo', pathMatch: 'full' },
   { path: 'userInfo', component: User_infoComponent },
+  { path: 'userMedia', component: User_infoMediaComponent },
   { path: 'team', component: TeamComponent }
 ];
 
