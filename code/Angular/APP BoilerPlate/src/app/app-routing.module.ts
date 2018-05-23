e3b0855e9ab0594777bd6c16efd5616fd5b6d4b0
ArnaldoLucienTema <pywbal@hotmail.com>
2018-05-23 21:38:19 +0200

    profile news and fixes
    Former-commit-id: 68353b4c8d9f4e57b7ea08e38cd42eda4953ec1e
    Former-commit-id: f2b4a275ce23d99effa89c21fdd46dc154823553

--- code/Angular/APP BoilerPlate/src/app/app-routing.module.ts
+++ code/Angular/APP BoilerPlate/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AuthGuard} from './_guards/auth.guard';
 import { User_infoComponent } from "./user_info/user_info.component";
 import {TeamComponent} from './team/team.component';
 import {User_infoMediaComponent} from './user-info-media/user-info-media.component';
+import {User_infoStatsComponent} from './user-info-stats/user-info-stats.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/userInfo', pathMatch: 'full' },
   { path: 'userInfo', component: User_infoComponent },
   { path: 'userMedia', component: User_infoMediaComponent },
+  { path: 'userStats', component: User_infoStatsComponent },
   { path: 'team', component: TeamComponent }
 ];
 
