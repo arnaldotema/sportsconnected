0459bafb882083a4d1979105b703c9a085c93dba
Trindade Arnaldo (extern) ( U806264 ) <arnaldo.trindade@mobi.ch>
2018-05-26 10:20:06 +0200

    Bern stuff
    Former-commit-id: fcd8cf1e55352fc8c4f36b03cf05b700326764b1
    Former-commit-id: bb91ac8a26e7781251d93ccab65c0d4c00884733

--- code/Angular/APP BoilerPlate/src/app/_models/team_season.ts
+++ code/Angular/APP BoilerPlate/src/app/_models/team_season.ts
@@ -1,3 +1,16 @@
 
+import {Recommendation} from "./recommendation";
+
 export class Team_Season{
+  acronym: string;
+  avatar: {
+    type: string,
+    default: string
+  };
+  name: string;
+  full_name: string;
+  recommendations : {
+    total_recommendations: number;
+    last_recommendations: Recommendation[];
+  };
 }
--- code/Angular/APP BoilerPlate/src/app/_models/team_viewmodel.ts
+++ code/Angular/APP BoilerPlate/src/app/_models/team_viewmodel.ts
@@ -1,6 +1,6 @@
 import {Recommendation} from "./recommendation";
 
-export class TeamViewModel{
+export class TeamViewModel {
   acronym: string;
   avatar: {
     type: string,
@@ -8,9 +8,38 @@ export class TeamViewModel{
   };
   name: string;
   full_name: string;
-
-  recommendations : {
+  recommendations: {
     total_recommendations: number;
     last_recommendations: Recommendation[];
   };
+  roster: {
+    playes: [{
+        user_id: string;
+        name: string;
+        avatar: string;
+        age: number;
+        positions: string[];
+        nationality: string;
+        residence: string;
+        stats: {
+          games: number,
+          goals: number,
+          assists: number,
+          yellow_cards: number,
+          red_cards: number,
+          minutes_played: number
+        };
+      }];
+    staff: [{
+      user_id: string;
+      name: string;
+      avatar: string;
+      age: number;
+      position: string;
+      nationality: string;
+      residence: string;
+    }]
+  };
+
+
 }
