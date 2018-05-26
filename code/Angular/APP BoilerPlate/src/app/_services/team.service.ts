774070eb902b261e8875ae9851d150ff4f0de17f
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-05-26 10:01:07 +0100

    Roster mock data
    Former-commit-id: a71d4fe773c3d4f199c04d3e15bbc52727753e9b
    Former-commit-id: aeb297c6be168320d1ad37d2c86980178b37786d

--- code/Angular/APP BoilerPlate/src/app/_services/team.service.ts
+++ code/Angular/APP BoilerPlate/src/app/_services/team.service.ts
@@ -55,11 +55,385 @@ export class TeamService {
       },
       roster: {
         players: [
-
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          },
+          {
+            user_id: '1',
+            name: 'Diogo Pires',
+            avatar: 'https://connectnigeria.com/articles/wp-content/uploads/2017/12/Arsenal-legend-Thierry-Henry-624927.jpg',
+            age: 25,
+            positions: ['Médio Centro', 'Médio Defensivo', 'Defesa Central'],
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+            stats: {
+              games: 15,
+              goals: 13,
+              assists: 4,
+              yellow_cards: 1,
+              red_cards: 0,
+              minutes_played: 1050
+            }
+          }
         ],
-        staff: {
-          
-        }
+        staff: [
+          {
+            user_id: '5',
+            name: 'José Mourinho',
+            avatar: 'https://cdn.images.dailystar.co.uk/dynamic/58/photos/763000/620x/Jose-Mourinho-644644.jpg',
+            age: 50,
+            position: 'Treinador principal',
+            nationality: 'Portuguesa',
+            residence: 'Manchester',
+          },
+          {
+            user_id: '6',
+            name: 'Jorge Jesus',
+            avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Jorge_Jesus.jpg/1200px-Jorge_Jesus.jpg',
+            age: 49,
+            position: 'Treinador adjunto',
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+          },
+          {
+            user_id: '4',
+            name: 'Vital de Carvalho',
+            avatar: 'https://openminds.swissre.com/static//images/profile-default.png',
+            age: 35,
+            position: 'Treinador adjunto',
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+          },
+          {
+            user_id: '3',
+            name: 'Nuno Carmo',
+            avatar: 'https://scontent.fopo3-1.fna.fbcdn.net/v/t31.0-8/15541052_10212069863200855_2889012374229061166_o.jpg?_nc_cat=0&oh=5b128be1ebf4151ec5aa2afb671b72d0&oe=5B8C9375',
+            age: 27,
+            position: 'Preparador físico',
+            nationality: 'Portuguesa',
+            residence: 'Lisboa',
+          },
+        ]
       }
 
     }
--- code/Angular/APP BoilerPlate/src/app/app-routing.module.ts
+++ code/Angular/APP BoilerPlate/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { User_infoComponent } from "./user_info/user_info.component";
 import {TeamComponent} from './team/team.component';
 import {User_infoMediaComponent} from './user-info-media/user-info-media.component';
 import {User_infoStatsComponent} from './user-info-stats/user-info-stats.component';
+import {TeamRosterComponent} from './team-roster/team-roster.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/userInfo', pathMatch: 'full' },
   { path: 'userInfo', component: User_infoComponent },
   { path: 'userMedia', component: User_infoMediaComponent },
   { path: 'userStats', component: User_infoStatsComponent },
-  { path: 'team', component: TeamComponent }
+  { path: 'team', component: TeamComponent },
+  { path: 'teamRoster', component: TeamRosterComponent}
 ];
 
 @NgModule({
