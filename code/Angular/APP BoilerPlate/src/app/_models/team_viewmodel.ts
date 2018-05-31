7f73a4ea6912cb188c6a933e786f3803667df94c
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-05-31 17:15:00 +0100

    Player & Team profile; Tryouts; colors; Getting stuff from service
    Former-commit-id: b46a212c9f2ee11c8632601059f3062e6e3c27e0
    Former-commit-id: 5d66a071bf717b3c4f613eff197d957b569bc6e4

--- code/Angular/APP BoilerPlate/src/app/_models/team_viewmodel.ts
+++ code/Angular/APP BoilerPlate/src/app/_models/team_viewmodel.ts
@@ -1,13 +1,51 @@
-import {Recommendation} from "./recommendation";
+import {Recommendation} from './recommendation';
 
 export class TeamViewModel {
   acronym: string;
-  avatar: {
-    type: string,
-    default: string
-  };
+  avatar: string;
   name: string;
   full_name: string;
+  current_season: {
+    season_id: string,
+    name: string,
+    stats: [{
+      competition_name: string,
+      competition_avatar: string,
+      games: number,
+      classification: string,
+      wins: number,
+      losses: number,
+      draws: number,
+    }]
+  };
+  tryouts: [{
+    address: string;
+    age_group: string;
+    days: string;
+    time: string;
+    requirements: string;
+  }];
+  personal_info: {
+    site: string;
+    email: string;
+    phone_number: string;
+    address: string;
+    president: string;
+    vice_president: string;
+    sports_director: string;
+    number_of_teams: number;
+    number_of_athletes: number;
+    number_of_coaches: number;
+    number_of_physiotherapists: number;
+    number_of_grass_fields: number;
+    number_of_synthetic_fields: number;
+    number_of_locker_rooms: number;
+    sponsors: [{
+      link: string;
+      name: string
+    }];
+    other_sports: string[]
+  };
   recommendations: {
     list: number[],
     top_5: [
@@ -29,23 +67,23 @@ export class TeamViewModel {
   };
   roster: {
     players: [{
-        user_id: string;
-        name: string;
-        avatar: string;
-        age: number;
-        number: number;
-        positions: string[];
-        nationality: string;
-        residence: string;
-        stats: {
-          games: number,
-          goals: number,
-          assists: number,
-          yellow_cards: number,
-          red_cards: number,
-          minutes_played: number
-        };
-      }];
+      user_id: string;
+      name: string;
+      avatar: string;
+      age: number;
+      number: number;
+      positions: string[];
+      nationality: string;
+      residence: string;
+      stats: {
+        games: number,
+        goals: number,
+        assists: number,
+        yellow_cards: number,
+        red_cards: number,
+        minutes_played: number
+      };
+    }];
     staff: [{
       user_id: string;
       name: string;
