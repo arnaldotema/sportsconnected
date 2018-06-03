898ea3373e58522a389390203adfc9ed1cb313da
Diogo Pires <dc.pires@campus.fct.unl.pt>
2018-06-03 02:30:41 +0100

    deployment to server
    Former-commit-id: 31de663e729a7bf2ae3d00ef59be3b15199609d5
    Former-commit-id: 2c3511c7c2ba070e75278dd2c3d3b444cda5d746

--- code/Website/models/football_team.js
+++ code/Website/models/football_team.js
@@ -3,12 +3,90 @@ var Schema   = mongoose.Schema;
 
 var FootballTeamSchema = new Schema({
     acronym: String,
-    avatar: {
-        type: String,
-        default: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg'
+    avatar: String,
+    name: String,
+    full_name: String,
+    current_season: {
+        season_id: String,
+        name: String,
+        stats: [{
+            competition_name: String,
+            competition_avatar: String,
+            games: Number,
+            classification: String,
+            wins: Number,
+            losses: Number,
+            draws: Number,
+        }]
     },
-    name: { type: String, required: true, unique: true},
-    full_name: { type: String, unique: true},
+    tryouts: [{
+        address: String,
+        age_group: String,
+        days: String,
+        time: String,
+        requirements: String,
+    }],
+    personal_info: {
+        site: String,
+        email: String,
+        phone_Number: String,
+        address: String,
+        president: String,
+        vice_president: String,
+        sports_director: String,
+        number_of_teams: Number,
+        number_of_athletes: Number,
+        number_of_coaches: Number,
+        number_of_physiotherapists: Number,
+        number_of_grass_fields: Number,
+        number_of_synthetic_fields: Number,
+        number_of_locker_rooms: Number,
+        sponsors: [{
+            link: String,
+            name: String
+        }],
+        other_sports: [String]
+    },
+    recommendations: {
+        list: [Number],
+            top_5: [
+            {
+                author: {
+                    name: String,
+                    id: Number,
+                    avatar: String,
+                    team: {
+                        id: String,
+                        acronym: String,
+                        avatar: String,
+                        name: String,
+                    },
+                },
+                text: String,
+            }
+        ],
+    },
+    roster: {
+        players: [{
+            user_id: String,
+            name: String,
+            avatar: String,
+            age: Number,
+            Number: Number,
+            positions: [String],
+            nationality: String,
+            residence: String
+        }],
+        staff: [{
+            user_id: String,
+            name: String,
+            avatar: String,
+            age: Number,
+            position: String,
+            nationality: String,
+            residence: String
+        }]
+    },
     external_ids: {
         zerozero: String,
     }
