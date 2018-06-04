2dd79ffb1b6e3e7a0e688b4e2a5dc24e869ab433
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-06-04 23:22:01 +0100

    Mock Match
    Former-commit-id: f61d3f2eebec30eb4540c687b6308c36a9e7a836
    Former-commit-id: 1bcb7a96eac6079c1d7baaea7e4ab9cdd77c72a6

--- code/Angular/APP BoilerPlate/src/app/match/match.component.ts
+++ code/Angular/APP BoilerPlate/src/app/match/match.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {User_infoService} from '../_services/user_info.service';
+import { Chart } from 'chart.js';
+import {MatDialog} from '@angular/material';
+import {MatchViewModel} from '../_models/match_viewmodel';
+import {MatchService} from '../_services/match.service';
 
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
   styleUrls: ['./match.component.css']
 })
-export class MatchComponent implements OnInit {
+export class MatchComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  viewModel: MatchViewModel;
+  matchService : MatchService;
+  constructor(/*private userInfoService: User_infoService, */public dialog: MatDialog) {
+    this.matchService = new MatchService();
+    this.viewModel = this.matchService.getMatch('0');
   }
 
+  ngOnInit() {}
+
+  ngAfterViewInit(){}
 }
