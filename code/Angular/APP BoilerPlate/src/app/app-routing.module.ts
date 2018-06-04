53fe4d5b8eb58192fa5924f3bb389d5828ac0b2e
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-06-04 23:22:01 +0100

    Mock Match
    Former-commit-id: 3fe1e2cf1af39da205e62e3234febfc23e8a4f99
    Former-commit-id: 9e41b3f960b41e12c5791c9b989d20120beb91ad

--- code/Angular/APP BoilerPlate/src/app/app-routing.module.ts
+++ code/Angular/APP BoilerPlate/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AuthGuard} from './_guards/auth.guard';
 import { User_infoComponent } from "./user_info/user_info.component";
 import {TeamComponent} from './team/team.component';
 import {HomeComponent} from './home/home.component';
+import {MatchComponent} from './match/match.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/userInfo', pathMatch: 'full' },
   { path: 'userInfo', component: User_infoComponent },
   { path: 'team', component: TeamComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'match', component: MatchComponent },
 ];
 
 @NgModule({
