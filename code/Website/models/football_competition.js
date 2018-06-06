9b96b2e7cfe4de6607adfb89760234ea0f0f9bad
Diogo Pires <dc.pires@campus.fct.unl.pt>
2018-06-06 10:05:51 +0100

    Arnaldo wuz here
    Former-commit-id: 64af92ec484c81574186c3e3c85ca3085fb42bc2
    Former-commit-id: a7d4775780349dc6e12324112046c57e939f7ead

--- code/Website/models/football_competition.js
+++ code/Website/models/football_competition.js
@@ -3,12 +3,44 @@ var Schema   = mongoose.Schema;
 
 var FootballCompetitionSchema = new Schema({
 	name : String,
+	avatar: String,
 	current_season: {
-        matchdays : Array,
-        standings : Array,
-        stats : Array
+        standings : [
+			{
+				id: {type: Schema.Types.ObjectId, ref: 'football_team'},
+				name: String,
+				avatar: String,
+				position: Number,
+				points: {type: Number, default: 0},
+                matches: {type: Number, default: 0},
+				wins: {type: Number, default: 0},
+				draws: {type: Number, default: 0},
+				losses: {type: Number, default: 0},
+				goals: {type: Number, default: 0},
+				goals_taken: {type: Number, default: 0}
+			}
+		],
+        stats : [
+            {
+                info_id: {type: Schema.Types.ObjectId, ref: 'football_user_info'},
+                name: String,
+                avatar: String,
+                goals: {type: Number, default: 0},
+                assists: {type: Number, default: 0},
+                minutes: {type: Number, default: 0},
+                wins: {type: Number, default: 0},
+                draws: {type: Number, default: 0},
+                losses: {type: Number, default: 0},
+				yellow_cards: {type: Number, default: 0},
+				red_cards: {type: Number, default: 0},
+                goals_taken: {type: Number, default: 0}
+            }
+		]
 	},
-	previous_seasons: [{type: Schema.Types.ObjectId, ref: 'football_competition_season'}]
+	previous_seasons: [{type: Schema.Types.ObjectId, ref: 'football_competition_season'}],
+    external_ids: {
+        zerozero: {type: Number, required: true, unique: true, index: true},
+    }
 });
 
 module.exports = mongoose.model('football_competition', FootballCompetitionSchema);
