9ab5775ed9a1e52b443e5c07f146b57df2858349
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-06-08 13:58:56 +0100

    create acc
    Former-commit-id: b21d5cf56a6898d9a09206f3a5fccb2a1aaeb695
    Former-commit-id: 7c2f99ddac1f6f730f6e50567a11bca67dc945ff

--- code/Angular/APP BoilerPlate/src/app/create-account/create-account.component.ts
+++ code/Angular/APP BoilerPlate/src/app/create-account/create-account.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {TeamService} from '../_services/team.service';
+import {UserInfoService} from '../_services/user_info.service';
+import {GenericUserService} from '../_services/generic_user.service';
+import {Search_entity_viewmodel} from '../_models/search_entity_viewmodel';
+import {TeamViewModel} from '../_models/team_viewmodel';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
+  teamService: TeamService;
+  genericUserService: GenericUserService;
+  userInfoService: UserInfoService;
+  teams: Search_entity_viewmodel;
+  team: TeamViewModel;
+  user;
+
   constructor() { }
 
   ngOnInit() {
+    this.teamService = new TeamService();
+    this.genericUserService = new GenericUserService();
+    this.userInfoService = new UserInfoService();
+    this.genericUserService.searchUser('','','team')
+      .subscribe(teams => this.teams = teams);
   }
+
+  getPlayer(id: string){
+    this.userInfoService.getUserInfo(id)
+      .subscribe(user => this.user = user);
+  }
+
+  getTeam (id: string){
+    this.teamService.getTeam(id)
+      .subscribe(team => this.team = team);
+  }
+
+  createPlayer (){
+
+  }
+
 
 }
