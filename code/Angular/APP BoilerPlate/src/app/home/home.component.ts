a8a33f20ca803c094c85e4696582ddcada38184e
ArnaldoLucienTema <pywbal@hotmail.com>
2018-06-10 14:05:48 +0100

    Changes after Belas FB 1
    Former-commit-id: 580c364a5a979b9bdda1869203d8fef369575dba
    Former-commit-id: 6c5aed8502da5d368b81159fd5d34b7dd178d325

--- code/Angular/APP BoilerPlate/src/app/home/home.component.ts
+++ code/Angular/APP BoilerPlate/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(
-  ) {
+  constructor(private router: Router ) {
 
   }
 
   ngOnInit() {
+  }
+
+  createAccount() {
+    this.router.navigate(['/create-account']);
   }
 }
