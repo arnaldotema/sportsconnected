a3bd95a2b1bf1b97f5655926110167da8cb375b5
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-06-19 02:15:52 +0100

    Player edition Matches
    Former-commit-id: 2707991a295024a66caca2a20a605d668bf4feec
    Former-commit-id: 751802361f47b6cbd104fda5f94bb33278471532

--- code/Angular/APP BoilerPlate/src/app/validators/index.ts
+++ code/Angular/APP BoilerPlate/src/app/validators/index.ts
@@ -1,4 +1,3 @@
 export { Country } from './country.model';
 export { PasswordValidator, ParentErrorStateMatcher } from './password.validator';
-export { PhoneValidator } from './phone.validator';
 export { UsernameValidator } from './username.validator';
