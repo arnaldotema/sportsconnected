8db605d60b396d0f323b3a1d1f9ab15f79cc2788
ArnaldoLucienTema <pywbal@hotmail.com>
2018-06-26 19:00:15 +0100

    modals done
    Former-commit-id: 53ac4b512463390c2c0a9762b2cb5ee006e03cb4
    Former-commit-id: b183d6f4b93c85fe78b1c3f97fb7aa038c1d93c8

--- code/Angular/APP BoilerPlate/src/app/_modals/recommendation-modal/recommendation-modal.component.spec.ts
+++ code/Angular/APP BoilerPlate/src/app/_modals/recommendation-modal/recommendation-modal.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecommendationModalComponent } from './recommendation-modal.component';
 
-describe('RecommendationModalComponent', () => {
+describe('TryoutModalComponent', () => {
   let component: RecommendationModalComponent;
   let fixture: ComponentFixture<RecommendationModalComponent>;
 
