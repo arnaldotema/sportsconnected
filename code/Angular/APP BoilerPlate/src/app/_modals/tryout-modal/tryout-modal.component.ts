9c1546dc38e8861b4612d5bffc1796017559ce14
ArnaldoLucienTema <pywbal@hotmail.com>
2018-06-26 18:45:11 +0100

    modals done
    Former-commit-id: 3924d6a89f6cc41dfb6353dc5360da809cb51cce
    Former-commit-id: 77a453d4966379b08481abe8b14a7a824c825a92

---
+++ code/Angular/APP BoilerPlate/src/app/_modals/tryout-modal/tryout-modal.component.ts
@@ -0,0 +1,54 @@
+import {Component, Inject, OnInit, AfterViewInit, ViewChild} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {TeamService} from '../../_services/team.service';
+import {TeamViewModel} from '../../_models/team_viewmodel';
+
+@Component({
+  selector: 'app-recommendation-modal',
+  templateUrl: './recommendation-modal.component.html',
+  styleUrls: ['./recommendation-modal.component.css']
+})
+export class RecommendationModalComponent implements OnInit {
+
+  text: string;
+  recommendationIdx: string;
+  edit: false;
+  create: true;
+  target;
+  author;
+
+  constructor(
+    public dialogRef: MatDialogRef<RecommendationModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    this.target = this.data.target;
+    debugger;
+    if (this.data.author != null)
+      this.author = this.data.author;
+    this.text = '';
+
+    /*
+    if (this.data.recommendationIdx != null) {
+      this.edit = true;
+      this.recommendationIdx = this.data.recommendationIdx;
+      this.text = this.data.recommendations[this.data.recommendationIdx];
+    }
+    */
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateClick(): void {
+    debugger;
+    this.dialogRef.close({
+      author : this.author,
+      text: this.text
+    });
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
