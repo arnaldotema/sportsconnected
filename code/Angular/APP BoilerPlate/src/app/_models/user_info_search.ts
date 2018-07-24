09094a0562184262b88d22b869db1fbf0f464ab7
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-07-24 23:41:16 +0100

    Search players button
    Former-commit-id: b5ed08984e812ba50c7754e48e680e6ec2f55014
    Former-commit-id: c409358112587e1b372f58836adf168e06d28337

---
+++ code/Angular/APP BoilerPlate/src/app/_models/user_info_search.ts
@@ -0,0 +1,56 @@
+import {Achievement} from './achievement';
+
+export class UserInfoSearch {
+  user_id: string;
+  personal_info: {
+    name: string,
+    age: number,
+    avatar: string,
+    full_name: string,
+    positions: string[],
+    height: number,
+    weight: number,
+    date_of_birth: string,
+    foot: string,
+    nationality: string,
+    residence: string,
+    updated_at: string
+  };
+  followers : number [];
+  current_season: {
+    season_id: number,
+    name: string,
+    team: {
+      id: number,
+      acronym: string,
+      avatar: string,
+      name: string,
+      full_name: string
+    },
+    stats: [{
+      season: string,
+      competition_name: string,
+      competition_avatar: string,
+      games: number,
+      wins: number,
+      losses: number,
+      draws: number,
+      goals: number,
+      assists: number,
+      yellow_cards: number,
+      red_cards: number,
+      minutes_played: number
+    }],
+    games: number[]
+  };
+  previous_seasons: number[];
+  skill_set: [{
+      name: string,
+      avatar: string,
+      endorsements: number[],
+    }];
+  recommendations: {
+    list: number[]
+  };
+  achievements: Achievement[];
+}
