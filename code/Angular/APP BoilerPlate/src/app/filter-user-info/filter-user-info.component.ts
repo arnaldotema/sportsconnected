64f44d5e908a56ff5651a8f68aeea4e35239f225
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-08-02 19:48:40 +0100

    Filter Search with custom filters per field
    Former-commit-id: 732da1008e126bd98aa5fc2334e308d615d3a7b2
    Former-commit-id: 8b1b4a66e4498d059e9455815035ff79cede4085

--- code/Angular/APP BoilerPlate/src/app/filter-user-info/filter-user-info.component.ts
+++ code/Angular/APP BoilerPlate/src/app/filter-user-info/filter-user-info.component.ts
@@ -55,6 +55,8 @@ export class FilterUserInfoComponent implements OnInit {
   sortedData;
   sliderRange;
 
+  value_types;
+
   private search_fields: Array<any> = [];
 
   constructor(private router: Router) {
@@ -64,59 +66,103 @@ export class FilterUserInfoComponent implements OnInit {
     this.teamService = new TeamService();
     this.genericUserService = new GenericUserService();
     this.userInfoService = new UserInfoService();
-    this.personal_data = ['Nome', 'D. de Nascimento', 'Idade', 'Residência', 'Clube', 'Mobilidade', 'Posição'];
-    this.stats = ['Jogos', 'Minutos', 'Golos', 'Assistências', 'Class. média', 'C. Amarelos', 'C. Vermelhos'];
+    this.personal_data = ['D. de Nascimento', 'Idade', 'Residência', 'Mobilidade', 'Posição'];
+    this.stats = ['Época', 'Jogos', 'Minutos', 'Golos', 'Assistências', 'Class. média', 'C. Amarelos', 'C. Vermelhos'];
     this.physical_atts = ['Altura', 'Peso', 'Votação SC', 'Velocidade', 'Resistência', 'Força', 'Agilidade', 'Reflexos', 'Impulsão', 'Proteção de bola', 'Corpo a corpo'];
     this.technical_atts = ['Passe', 'Recepção', 'Drible', 'Remates Longe', 'Finalização', 'Cabeceamento', 'Cruzamento', 'Desarme', 'Primeiro toque', 'Lançamentos Laterais', 'Cantos', 'Livres diretos', 'Livres indiretos', 'Penalties'];
     this.mental_atts = ['Agressividade', 'Bravura', 'Antecipação', 'Concentração', 'Determinação', 'Compostura', 'Tomada de decisão', 'Criatividade', 'Jogo sem bola', 'Posicionamento', 'Velocidade Reação', 'Visão de Jogo', 'Disciplina', 'Ética de trabalho', 'Espírito de grupo', 'Liderança', 'Comunicação'];
     this.sc_atts = ['Badges', 'Badges Época', 'Badges Carreira'];
     this.competitions = ['Escalão', 'Distrito', 'Divisão', 'Clube'];
-    this.seasons = ['Todas', '2018/2019', '2017/2018', '2016/2017', '2015/2016', '2014/2015'];
 
-    this.number_filter_types = [
-      'Exatamente', 'Pelo menos', 'No máximo', 'Entre'
-    ];
-    this.mock_value_types = [
-      '1', '2', '3', '4', '5','6','7', '8', '9', '10', '20', '25'
-    ];
+    this.value_types = {
+      'default': {
+        values: Array.apply(null, {length: 50}).map(Number.call, Number),
+        unit: '',
+        filters: ['Exatamente', 'Pelo menos', 'No máximo', 'Entre']
 
+      },
+      'birth_date': {
+        values: ['1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009'],
+        unit: '',
+        filters: ['Exatamente', 'Desde', 'Até', 'Entre']
+      },
+      'age': {
+        values: Array.apply(null, {length: 35}).map(Number.call, Number),
+        unit: 'anos',
+        filters: ['Exatamente', 'Pelo menos', 'No máximo', 'Entre']
+      },
+      'residence': {
+        values: ['Lisboa', 'Porto', 'Coimbra', 'Algarve', 'Setúbal', 'Santarém', 'Viseu', 'Leiria'],
+        unit: '',
+        filters: ['Reside em']
+      },
+      'team': {
+        values: ['Seixal FC', 'SL Benfica', 'FC Porto', 'Amora FC', 'Alcochetense', 'Montijo', 'Sporting CP'],
+        unit: '',
+        filters: ['Joga no']
+      },
+      'mobility': {
+        values: ['Total', 'Regional', 'Distrital'],
+        unit: '',
+        filters: ['No máximo']
+      },
+      'position': {
+        values: ['Guarda-redes', 'Defesa Central', 'Defesa Esquerdo', 'Defesa Direito', 'Defesa', 'Médio Defensivo', 'Médio Ofensivo', 'Médio Esquerdo', 'Médio Direito', 'Médio Centro', 'Ala Esquerdo', 'Ala Direito', 'Avançado',],
+        unit: '',
+        filters: ['É natural como'],
+      },
+      'games': {
+        values: Array.apply(null, {length: 50}).map(Number.call, Number),
+        unit: 'jogos',
+        filters: ['Exatamente', 'Pelo menos', 'No máximo', 'Entre']
+      },
+      'minutes': {
+        values: Array.apply(null, {length: 90}).map(Number.call, Number),
+        unit: 'mins',
+        filters: ['Exatamente', 'Pelo menos', 'No máximo', 'Entre']
+      },
+      'goals': {
+        values: Array.apply(null, {length: 100}).map(Number.call, Number),
+        unit: 'golos',
+        filters: ['Exatamente', 'Pelo menos', 'No máximo', 'Entre']
+      },
+      'assists': {
+        values: Array.apply(null, {length: 100}).map(Number.call, Number),
+        unit: 'ass.',
+        filters: ['Exatamente', 'Pelo menos', 'No máximo', 'Entre']
+      },
+      'height': {
+        values: ['1,20', '1,50', '1,70', '1,80', '1,90', '2,00'],
+        unit: 'm',
+        filters: ['Exatamente', 'Pelo menos', 'No máximo', 'Entre']
+      },
+      'weight': {
+        values: ['50', '60', '70', '80', '90'],
+        unit: 'kg',
+        filters: ['Exatamente', 'Pelo menos', 'No máximo', 'Entre']
+      },
+      'age_group': {
+        values: ['Petizes', 'Traquinas', 'Benjamins B', 'Benjamins A', 'Infantis B', 'Infantis A', 'Iniciados B', 'Iniciados', 'Juvenis B', 'Juvenis', 'Juniores B', 'Juniores', 'Seniores'],
+        unit: '',
+        filters: ['Exatamente', 'Desde', 'Até', 'Entre']
+      },
+      'district': {
+        values: ['Lisboa', 'Porto', 'Setúbal', 'Santarém', 'Faro'],
+        unit: '',
+        filters: ['Joga em/no']
+      },
 
-    this.games_filter_types = [
-      'Exatamente', 'Pelo menos', 'No máximo', 'Entre'
-    ];
-    this.games_value_types = [
-      '1', '2', '3', '4', '5','6','7', '8', '9', '10', '20', '25'
-    ];
-
-
-    this.position_filter_types = [
-      'É natural como'
-    ];
-    this.position_value_types = [
-      'Guarda-redes',
-      'Defesa Central',
-      'Defesa Esquerdo',
-      'Defesa Direito',
-      'Defesa',
-      'Médio Defensivo',
-      'Médio Ofensivo',
-      'Médio Esquerdo',
-      'Médio Direito',
-      'Médio Centro',
-      'Ala Esquerdo',
-      'Ala Direito',
-      'Avançado',
-    ];
-    this.residence_filter_types = [
-      'Reside em'
-    ];
-
-    this.residence_value_types = [
-      'Lisboa', 'Seixal', 'Porto', 'Coimbra', 'Algarve'
-    ];
-
-
-    this.sliderRange = [4, 80];
+      'division': {
+        values: ['Nacional', 'Regional', 'Distrital', '1ª Div', '2ª Div', '3ª Div'],
+        unit: '',
+        filters: ['Joga na']
+      },
+      'season': {
+        values: ['2018/2019', '2017/2018', '2016/2017', '2015/2016', '2014/2015'],
+        unit: '',
+        filters: ['Exatamente', 'Desde', 'Até', 'Entre']
+      }
+    };
   }
 
   loadAgeGroups() {
@@ -210,8 +256,8 @@ export class FilterUserInfoComponent implements OnInit {
     // Reset the selected fileds
     this.personal_data_select = undefined;
     this.competition_select = undefined;
-    this.physical_atts_select= undefined;
-    this.sc_atts_select= undefined;
+    this.physical_atts_select = undefined;
+    this.sc_atts_select = undefined;
     this.technical_atts_select = undefined;
 
   }
@@ -240,21 +286,84 @@ export class FilterUserInfoComponent implements OnInit {
 
   addFieldValue(form_values) {
     form_values.forEach((form_value, key) => {
-      if (!this.search_fields.some(item => item.form == form_value)){
+      if (!this.search_fields.some(item => item.form == form_value)) {
+        let mapped_var = mapVariable(form_value);
+        debugger;
+        let obj = this.value_types[mapped_var] ? this.value_types[mapped_var] : this.value_types['default'];
         this.search_fields.push({
           form: form_value,
-          filter: {},
-          value: {}
+          filters: obj.filters,
+          values: obj.values,
+          selected_filter: {},
+          selected_value: {}
         });
       }
     });
-
   }
 
   deleteFieldValue(index) {
     this.search_fields.splice(index, 1);
   }
 
+}
+
+function mapVariable(text) {
+  let translated = '';
+
+  switch (text) {
+    case 'Posição':
+      translated = 'position';
+      break;
+    case 'Idade':
+      translated = 'age';
+      break;
+    case 'Residência':
+      translated = 'residence';
+      break;
+    case 'Clube':
+      translated = 'team';
+      break;
+    case 'Mobilidade':
+      translated = 'mobility';
+      break;
+    case 'Época':
+      translated = 'season';
+      break;
+    case 'Jogos':
+      translated = 'games';
+      break;
+    case 'D. de Nascimento':
+      translated = 'birth_date';
+      break;
+    case 'Minutos':
+      translated = 'minutes';
+      break;
+    case 'Golos':
+      translated = 'goals';
+      break;
+    case 'Assistências':
+      translated = 'assists';
+      break;
+    case 'Altura':
+      translated = 'height';
+      break;
+    case 'Peso':
+      translated = 'weight';
+      break;
+    case 'Escalão':
+      translated = 'age_group';
+      break;
+    case 'Distrito':
+      translated = 'district';
+      break;
+    case 'Divisão':
+      translated = 'division';
+      break;
+    default:
+      translated = '';
+  }
+
+  return translated;
 }
 
 function compare(a, b, isAsc) {
