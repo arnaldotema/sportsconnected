6d50631fa4747d73226f8a9ce0785d21b16c57df
Diogo Pires <dc.pires@campus.fct.unl.pt>
2018-08-06 18:25:49 +0100

    Models to front-end integrate
    Former-commit-id: 3e6420ff5a80051cc1196ffca36be84ce0233312
    Former-commit-id: e3c324ffda755f568dbd1eeebc9d45579246aca5

---
+++ code/Website/models/football_achievement.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var FootballCompetitionSchema = new Schema({
+	name : String,
+	avatar: String,
+	current_season: {type: Schema.Types.ObjectId, ref: 'football_competition_season'},
+	previous_seasons: [{type: Schema.Types.ObjectId, ref: 'football_competition_season'}],
+    external_ids: {
+        zerozero: {type: Number, required: true, unique: true, index: true},
+    }
+});
+
+FootballCompetitionSchema.statics = require('../services/football_competition_service');
+
+module.exports = mongoose.model('football_competition', FootballCompetitionSchema);
