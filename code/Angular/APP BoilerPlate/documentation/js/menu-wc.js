ff309b49dec310930a40fae51cb6483537644619
Trindade Arnaldo (extern) <arnaldo.trindade@mobi.ch>
2018-08-30 23:27:45 +0100

    Documentation
    Former-commit-id: f8700c5a54ea50138fcb057e397901bd8a58938c
    Former-commit-id: eb6ed089bafcdc4aee2362193d3cb7e828e3ae2b

--- code/Angular/APP BoilerPlate/documentation/js/menu-wc.js
+++ code/Angular/APP BoilerPlate/documentation/js/menu-wc.js
@@ -14,7 +14,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         let tp = lithtml.html(`<nav>
     <ul class="list">
         <li class="title">
-            <a href="index.html" data-type="index-link">IBB documentation</a>
+            <a href="index.html" data-type="index-link">Players Net documentation</a>
         </li>
         <li class="divider"></li>
         ${ isNormalMode ? `<div id="book-search-input" role="search">
