aca0500671d5533de977334e97281dd50b042883
ArnaldoLucienTema <arnaldo.trindade@closer>
2018-09-29 19:39:49 +0100

    Making data persistent for user voting, recommendation and  profile aggregation. Also chaning the logic within what's kept in the user session token
    Former-commit-id: 6fa6f784077f1b97804c137cb051c38573959d00
    Former-commit-id: b5e1a459e9776e14b9aa202791460a4d524aa7a6

--- code/Website/services/football_user_info_service.js
+++ code/Website/services/football_user_info_service.js
@@ -1,13 +1,15 @@
 const logger = require('../logging');
 const _ = require('underscore');
 
-const updateAndReturnByZeroZeroId = function(zerozero_id, user_info, cb) {
+let Service = {};
+
+Service.updateAndReturnByZeroZeroId = function(zerozero_id, user_info, cb) {
     const query = {"external_ids.zerozero": zerozero_id};
 
     this.findOneAndUpdate(query, user_info, { upsert:true, new:true, setDefaultsOnInsert: true }, cb);
 };
 
-const updateUserInfosCurrentSeason = function (seasons, cb) {
+Service.updateUserInfosCurrentSeason = function (seasons, cb) {
     let operations = [];
 
     seasons.forEach(function(season){
@@ -29,11 +31,121 @@ const updateUserInfosCurrentSeason = function (seasons, cb) {
             }
         });
     });
+    this.bulkWrite(operations, {}, cb);
+}
+
+Service.addRecommendation = function (recommendation, user_info_id, cb) {
+    let operations = [];
+
+    operations.push({
+        updateOne: {
+            filter: {
+                "_id": user_info_id
+            },
+            update: {
+                $push :  {
+                    "recommendations.list": recommendation
+                }
+            }
+        }
+    });
 
     this.bulkWrite(operations, {}, cb);
 }
 
-module.exports = {
-    updateAndReturnByZeroZeroId: updateAndReturnByZeroZeroId,
-    updateUserInfosCurrentSeason: updateUserInfosCurrentSeason
+Service.editRecommendation = function (recommendation, user_info_id, cb) {}
+
+Service.deleteRecommendation = function (recommendation, user_info_id, cb) {}
+
+Service.addSkillVote = function (skill_name, author_user_id, user_info_id, cb) {
+
+    let operations = [];
+
+    operations.push({
+        updateOne: {
+            filter: {
+                "_id": user_info_id
+            },
+            updateOne: {
+                filter: {
+                    "skill_set.name": skill_name
+                },
+                update: {
+                    $push : {
+                        "endorsments": author_user_id
+                    }
+                }
+            }
+        }
+    });
+
+    this.bulkWrite(operations, {}, cb);
 }
+
+Service.follow = function (follower_id, user_info_id, cb) {
+
+    let operations = [];
+
+    operations.push({
+        updateOne: {
+            filter: {
+                "_id": user_info_id
+            },
+            update: {
+                $push : {
+                    "followers": follower_id
+                }
+            }
+        }
+    });
+
+    operations.push({
+        updateOne: {
+            filter: {
+                "_id": follower_id
+            },
+            update: {
+                $push : {
+                    "following": user_info_id
+                }
+            }
+        }
+    });
+
+    this.bulkWrite(operations, {}, cb);
+}
+
+Service.unfollow = function (unfollower_id, user_info_id, cb) {
+
+    let operations = [];
+
+    operations.push({
+        updateOne: {
+            filter: {
+                "_id": user_info_id
+            },
+            update: {
+                $pull : {
+                    "followers": {'id': unfollower_id}
+                }
+            }
+        }
+    });
+
+    operations.push({
+        updateOne: {
+            filter: {
+                "_id": unfollower_id
+            },
+            update: {
+                $pull : {
+                    "following": {'id': user_info_id}
+                }
+            }
+        }
+    });
+
+    this.bulkWrite(operations, {}, cb);
+}
+
+module.exports = Service;
