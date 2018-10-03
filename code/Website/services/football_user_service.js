7dbae723eeb97c286b8d4343cecf48b5b5c65294
ArnaldoLucienTema <arnaldo.trindade@closer>
2018-10-03 02:13:20 +0100

    Recommendation implementation; Session user logic; Front to Back data persistence implementation for recommendations; Back end tweaks; etc...
    Former-commit-id: 3e026b17b7104b6d7f4cf0a587e877b560121145
    Former-commit-id: 489ce84b93166c0bfb12be25a9ebdedec75f40c9

--- code/Website/services/football_user_service.js
+++ code/Website/services/football_user_service.js
@@ -3,23 +3,4 @@ const _ = require('underscore');
 
 let Service = {};
 
-Service.aggregateProfile = function (user_id, user_info_id, cb) {
-    let operations = [];
-
-    operations.push({
-        updateOne: {
-            filter: {
-                "_id": user_id
-            },
-            update: {
-                $push : {
-                    "player": user_info_id
-                }
-            }
-        }
-    });
-
-    this.bulkWrite(operations, {}, cb);
-};
-
 module.exports = Service;
