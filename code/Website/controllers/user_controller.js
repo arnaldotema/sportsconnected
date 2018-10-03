f59d2e7b5182231137e92d3ccc69b3cfd8e3f16b
ArnaldoLucienTema <arnaldo.trindade@closer>
2018-10-03 02:13:20 +0100

    Recommendation implementation; Session user logic; Front to Back data persistence implementation for recommendations; Back end tweaks; etc...
    Former-commit-id: 91590e2961018e6ff160480227db1d0d622db616
    Former-commit-id: 1a2349b6ea83d557f9156db66d5b7225048ffade

--- code/Website/controllers/user_controller.js
+++ code/Website/controllers/user_controller.js
@@ -1,4 +1,9 @@
 var UserModel = require('../models/football_user.js');
+var FootballUserInfo = require('../models/football_user_info.js');
+const Entities = require('html-entities').AllHtmlEntities;
+const entities = new Entities();
+const jwt = require('jsonwebtoken');
+
 
 /**
  * user_controller.js
@@ -96,6 +101,8 @@ Service.update = function (req, res) {
             });
         }
 
+        // TODO : Careful! This is obsolete, the user has no longer this fields
+
         User.email = req.body.email ? req.body.email : User.email;
         User.password = req.body.password ? req.body.password : User.password;
         User.last_login = req.body.last_login ? req.body.last_login : User.last_login;
@@ -139,23 +146,75 @@ Service.remove = function (req, res) {
 Service.aggregate_profile = function (req, res) {
     let id = req.params.id;
     let profile_id = req.body.id;
+    let user_session = {}
 
-    UserModel.aggregateProfile(profile_id, id, (err, user_info) => {
-        if (err) {
-            return res.status(500).json({
-                message: 'Error when aggregating user_info',
-                error: err
-            });
-        }
-        if (!user_info) {
-            return res.status(404).json({
-                message: 'No such user_info'
-            });
-        }
-        return res.json(user_info);
-    })
+    FootballUserInfo.findOne({_id: profile_id})
+        .populate('current_season')
+        .exec(function (err, user_info) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting user_info',
+                    error: err
+                });
+            }
+            if (!user_info) {
+                return res.status(404).json({
+                    message: 'No such user_info'
+                });
+            }
+
+            user_info = JSON.parse(entities.decode(JSON.stringify(user_info)));
+
+
+            UserModel.findOne({_id: id})
+                .exec(function (err, user) {
+                    if (err) {
+                        return res.status(500).json({
+                            message: 'Error when getting user',
+                            error: err
+                        });
+                    }
+                    if (!user) {
+                        return res.status(404).json({
+                            message: 'No such user'
+                        });
+                    }
+
+                    // Storing the aggregated profile to the current user
+                    user.profile_id = user_info._id;
+                    user.user_type = user_info.current_season.team ? 'football_user_info' : 'football_team';
+
+
+                    // Sending the user information based on the user aggregated profile
+                    // This information will be used as session user in the front end
+                    user_session.profile_id = user_info._id;
+                    user_session.user_type = user_info.current_season.team ? 'football_user_info' : 'football_team';
+                    user_session.avatar = user_info.current_season.personal_info.avatar;
+                    user_session.name = user_info.current_season.personal_info.name;
+                    user_session.team_avatar = user_info.current_season.team ? user_info.current_season.team.avatar : 'n/a';
+                    user_session.team_id = user_info.current_season.team ? user_info.current_season.team._id : 'n/a';
+                    user_session.team_acronym = user_info.current_season.team ? user_info.current_season.team.acronym : 'n/a';
+                    user_session.team_name = user_info.current_season.team ? user_info.current_season.team.name : 'n/a';
+
+                    user_session.token = jwt.sign({user: user_session}, 'top_secret');
+
+                    user.save(function (err, saved_user) {
+                        if (err) {
+                            return res.status(500).json({
+                                message: 'Error when updating user.',
+                                error: err
+                            });
+                        }
+
+                        // Sending the user information merged with the information from its aggregated user_info
+                        return res.json(user_session);
+                    });
+                });
+
+        });
+
+
 };
-
 
 
 module.exports = Service;
