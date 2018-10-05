0517df08d23317d0feeb0ca642726d4f41a9e8ae
ArnaldoLucienTema <arnaldo.trindade@closer>
2018-10-05 11:27:32 +0100

    following - working; voting - working; recommendations - working (partially)
    Former-commit-id: 8ea32042c832f3eeeac0a864437cf4090079b8a5
    Former-commit-id: 0942f342dbf259364c960a393fe482b0440bd213

--- code/Website/services/football_user_info_service.js
+++ code/Website/services/football_user_info_service.js
@@ -3,16 +3,16 @@ const _ = require('underscore');
 
 let Service = {};
 
-Service.updateAndReturnByZeroZeroId = function(zerozero_id, user_info, cb) {
+Service.updateAndReturnByZeroZeroId = function (zerozero_id, user_info, cb) {
     const query = {"external_ids.zerozero": zerozero_id};
 
-    this.findOneAndUpdate(query, user_info, { upsert:true, new:true, setDefaultsOnInsert: true }, cb);
+    this.findOneAndUpdate(query, user_info, {upsert: true, new: true, setDefaultsOnInsert: true}, cb);
 };
 
 Service.updateUserInfosCurrentSeason = function (seasons, cb) {
     let operations = [];
 
-    seasons.forEach(function(season){
+    seasons.forEach(function (season) {
         let user_info_season = season._doc;
 
         operations.push({
@@ -21,10 +21,10 @@ Service.updateUserInfosCurrentSeason = function (seasons, cb) {
                     "_id": user_info_season.user_info_id
                 },
                 update: {
-                    $set : {
+                    $set: {
                         "current_season": user_info_season._id
                     },
-                    $push : {
+                    $push: {
                         "previous_seasons": user_info_season._id
                     }
                 }
@@ -43,7 +43,7 @@ Service.addRecommendation = function (recommendation_id, user_info_id, cb) {
                 "_id": user_info_id
             },
             update: {
-                $push :  {
+                $push: {
                     "recommendations.list": recommendation_id
                 }
             }
@@ -53,34 +53,72 @@ Service.addRecommendation = function (recommendation_id, user_info_id, cb) {
     this.bulkWrite(operations, {}, cb);
 }
 
-Service.editRecommendation = function (recommendation, user_info_id, cb) {}
+Service.editRecommendation = function (recommendation, user_info_id, cb) {
+}
 
-Service.deleteRecommendation = function (recommendation, user_info_id, cb) {}
+Service.deleteRecommendation = function (recommendation, user_info_id, cb) {
+}
 
 Service.addSkillVote = function (skill_name, author_user_id, user_info_id, cb) {
 
-    let operations = [];
-
-    operations.push({
-        updateOne: {
-            filter: {
-                "_id": user_info_id
-            },
-            updateOne: {
-                filter: {
-                    "skill_set.name": skill_name
-                },
-                update: {
-                    $push : {
-                        "endorsments": author_user_id
-                    }
-                }
-            }
+    let conditions = {
+        "_id": user_info_id,
+        "skill_set.name": skill_name
+    };
+    let update = {
+        $push: {
+            "skill_set.$.endorsements": author_user_id
         }
-    });
+    };
 
-    this.bulkWrite(operations, {}, cb);
+    this.update(conditions, update, {}, cb);
 }
+
+Service.setAllSkillVotes = function (cb) {
+
+    let start_up_arrays = [
+        {
+            name: 'Goleador',
+            avatar: '/assets/scorer.png',
+            endorsements: [],
+        },
+        {
+            name: 'Drible',
+            avatar: '/assets/dribble.png',
+            endorsements: [],
+        },
+        {
+            name: 'Rapidez',
+            avatar: '/assets/fast.png',
+            endorsements: [],
+        },
+        {
+            name: 'Passador',
+            avatar: '/assets/passer.png',
+            endorsements: [],
+        },
+        {
+            name: 'Força',
+            avatar: '/assets/strong.png',
+            endorsements: [],
+        },
+        {
+            name: 'Muralha defensiva',
+            avatar: '/assets/defender.png',
+            endorsements: [],
+        }
+    ];
+
+    let conditions = {type: 1};
+    let update = {
+        $set: {
+            skill_set: start_up_arrays
+        }
+    };
+    let options = {multi: true};
+
+    this.update(conditions, update, options, cb);
+};
 
 Service.follow = function (follower_id, user_info_id, cb) {
 
@@ -92,7 +130,7 @@ Service.follow = function (follower_id, user_info_id, cb) {
                 "_id": user_info_id
             },
             update: {
-                $push : {
+                $push: {
                     "followers": follower_id
                 }
             }
@@ -105,7 +143,7 @@ Service.follow = function (follower_id, user_info_id, cb) {
                 "_id": follower_id
             },
             update: {
-                $push : {
+                $push: {
                     "following": user_info_id
                 }
             }
@@ -119,14 +157,15 @@ Service.unfollow = function (unfollower_id, user_info_id, cb) {
 
     let operations = [];
 
+
     operations.push({
         updateOne: {
             filter: {
                 "_id": user_info_id
             },
             update: {
-                $pull : {
-                    "followers": {'id': unfollower_id}
+                $pull: {
+                    "followers": unfollower_id
                 }
             }
         }
@@ -138,8 +177,8 @@ Service.unfollow = function (unfollower_id, user_info_id, cb) {
                 "_id": unfollower_id
             },
             update: {
-                $pull : {
-                    "following": {'id': user_info_id}
+                $pull: {
+                    "following": user_info_id
                 }
             }
         }
