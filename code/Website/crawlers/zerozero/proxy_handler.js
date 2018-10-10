245b8c3fa145293e7302395809e8b4d0d0e81eb1
ArnaldoLucienTema <arnaldo.trindade@closer>
2018-10-10 15:51:00 +0100

    Bug fixes and stuff
    Former-commit-id: f74764d8d067e8e12b8fb3fe462b1f17a6620e75
    Former-commit-id: 2ab2a774fd2394f53581fabc8c18b58ac9e3264a

--- code/Website/crawlers/zerozero/proxy_handler.js
+++ code/Website/crawlers/zerozero/proxy_handler.js
@@ -2,7 +2,7 @@ const logger = require('../../logging');
 
 const proxies = [undefined];
 const users = {
-    undefined: '0d8af940-19c9-4aa2-9f5c-4aac985991d5',
+    undefined: '48e0cceb-330a-4fcb-bc9e-ad3730af907e',
     'http://139.162.235.163:31028':'879739dc-74ce-4598-ad6e-6b540fd28af5'
 }
 
@@ -10,6 +10,9 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+/*
+ * Detecta certos erros no pedido e automaticamente muda de proxy (Caso haja mais proxies disponíveis)
+ */
 const crawl = function (err, res, done) {
     if( res.$(".g-recaptcha").length > 0){
         logger.error("Proxy failed at request: " + res.options.uri + " with PROXY: " + res.options.proxy);
