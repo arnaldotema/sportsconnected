64aa7abdf60e7c941d65721d52dd250ed672aa3f
ArnaldoLucienTema <pywbal@hotmail.com>
2018-10-12 08:37:29 +0100

    App Logo and names changed
    Former-commit-id: 5192f45a9fc61ede158cdf20e82d23b415b36251
    Former-commit-id: c2593994889187d260eed5b94f084e1c2358887c

--- code/Website/gary_processor/gary_processor.js
+++ code/Website/gary_processor/gary_processor.js
@@ -39,7 +39,7 @@ Service.updateRecommendations();
 let test = true;
 
 let regexScheduler = schedule.scheduleJob('*/20 * * * * *', function(fireDate){
-    logger.info('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
+    logger.info('This job was set to run at ' + fireDate + ', and actually ran at ' + new Date());
 
     footballUserInfo.getUserInfosByUpdatedAt(last_run, function (err, result) {
         if (err) {
