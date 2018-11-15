f3fa9ac941cfd9db81cefab3d86d5e163fe986d3
ArnaldoLucienTema <arnaldo.trindade@closer>
2018-11-15 14:45:47 +0000

    Changing condition to get Association info
    Former-commit-id: c295cdef351ba3aaad87baf99601561336426d85
    Former-commit-id: 3abf2c38047ee8d8acd84067d2e50ca776d993e8

--- code/Website/crawlers/zerozero/base_uris.js
+++ code/Website/crawlers/zerozero/base_uris.js
@@ -2,6 +2,7 @@ module.exports = {
     // COMPETITIONS
     ALL_COMPETITIONS: 'http://www.zerozero.pt/compet.php?continente=0&mod=1&gen=0&esc=0&stats=0&idpais=1',
     COMPETITION: 'http://www.zerozero.pt/competicao.php?id_comp={competition_id}',
+    ASSOCIATION: 'http://www.zerozero.pt/associacao.php?id_comp={competition_id}',
     COMPETITION_EDITION: 'http://www.zerozero.pt/edition.php?id={edition_id}',
     COMPETITION_EDITION_MATCHES: 'http://www.zerozero.pt/edition_matches.php?id={edition_id}&page={page}',
     // TEAMS
