fea767e3495d1c7e00d443e538ac696c77e35138
ArnaldoLucienTema <arnaldo.trindade@closer>
2018-11-25 13:08:33 +0000

    Mini search bar working for teams - fixed and fucntional but needs improvement
    Former-commit-id: 5820f2359e350a5164f74be803842daa135f2f9e
    Former-commit-id: a62d2111fe08fbf573ee5f232ade266982fe2fb1

--- code/Website/controllers/global_controller.js
+++ code/Website/controllers/global_controller.js
@@ -19,42 +19,45 @@ Service.search = function (req, res) {
     let team_select = {
         "_id": 1,
         "avatar": 1,
-        "name": 1
+        "name": 1,
+        "acronym": 1
     };
 
     let user_info_season_query = {
-        'personal_info.name' : {
-            '$regex' : req.body.query,
-            '$options' : 'i'
+        'personal_info.name': {
+            '$regex': req.body.query,
+            '$options': 'i'
         }
     };
 
     let team_query = {
-        'name' : {
-            '$regex' : req.body.query,
-            '$options' : 'i'
+        'name': {
+            '$regex': req.body.query,
+            '$options': 'i'
         }
     };
 
     let global_search_result = [];
 
-    /*
-      personal_info:{
-        name: string,
-        avatar: string
-      };
-      team: {
-        id: string,
-        acronym: string,
-        avatar: string,
-        name: string,
-        full_name: string
-      };
-      _id: string;
-      user_info_id: string;
-      type: string;
 
-    */
+    /*
+  personal_info:{
+    name: string,
+    avatar: string
+  };
+  team: {
+    id: string,
+    acronym: string,
+    avatar: string,
+    name: string,
+    full_name: string
+  };
+  _id: string;
+  user_info_id: string;
+  type: string;
+
+*/
+
 
     FootballUserInfoSeason
         .find(user_info_season_query)
@@ -69,7 +72,7 @@ Service.search = function (req, res) {
 
             user_infos = JSON.parse(entities.decode(JSON.stringify(user_infos)));
 
-            user_infos.forEach(user_info =>{
+            user_infos.forEach(user_info => {
                 user_info['type'] = 'user-info';
             });
 
@@ -88,13 +91,17 @@ Service.search = function (req, res) {
 
                     teams = JSON.parse(entities.decode(JSON.stringify(teams)));
 
-                    teams.forEach(team =>{
+                    teams.forEach(team => {
                         team['personal_info'] = {
-                            'name' : team.name,
-                            'avatar' : team.avatar
-                        }
+                            'name': team.name,
+                            'avatar': team.avatar
+                        };
                         team['user_info_id'] = team._id;
                         team['type'] = 'team';
+                        team['team'] = {
+                            'team_id': team._id,
+                            'name': team.acronym
+                        };
 
                     });
 
