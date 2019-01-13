aef0e7ce3199f6261832099c16d3bf9d270b4c2e
ArnaldoLucienTema <pywbal@hotmail.com>
2019-01-13 19:25:12 +0000

    chat models
    Former-commit-id: 2d3bb5c204a87b7f7078058180e6b8ee0f5c553d
    Former-commit-id: 63ceb562a8d7952090a13b5df3f8acb0e2c1c4f1

---
+++ code/server/models/chat_conversation.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ChatConversationSchema = new Schema({
+    id: Number,
+    participants: [{
+            name: String,
+            info_id: {type: Schema.Types.ObjectId, ref: 'football_user_info'},
+            avatar: String,
+        }],
+    updated_at: Date
+});
+
+ChatConversationSchema.statics = require('../services/chat_conversation_service');
+
+module.exports = mongoose.model('chat_conversation', ChatConversationSchema);
---
+++ code/server/models/chat_message.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MessageSchema = new Schema({
+    sender: {
+        name: String,
+        info_id: {type: Schema.Types.ObjectId, ref: 'football_user_info'},
+        avatar: String,
+    },
+    text: String,
+    time_created: Date,
+    conversation_id: {type: Schema.Types.ObjectId, ref: 'chat_conversation'},
+    deleted: {type: Boolean, required: true, index: true},
+    archived: {type: Boolean, required: true, index: true}
+});
+
+MessageSchema.statics = require('../services/chat_message_service');
+
+module.exports = mongoose.model('chat_message', MessageSchema);
