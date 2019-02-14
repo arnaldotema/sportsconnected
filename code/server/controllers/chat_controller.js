e15444e8e3f083248a6b02a8e8d60a9388a16214
ArnaldoLucienTema <arnaldo.trindade@closer>
2019-02-14 23:57:04 +0000

    Chat Backend
    Former-commit-id: 91157633fb07bc85e856a2aef59b99aa38929bf3
    Former-commit-id: 05a51a8386e6a941fd5ede2b9aee479fd27d9f55

--- code/server/controllers/chat_controller.js
+++ code/server/controllers/chat_controller.js
@@ -12,20 +12,66 @@ Service.loadConversations = function (req, res) {
 // use conversation service to get conversations for this user
 // .then() get messages for each conversation
 
+    let ids = req.params.id;
+
+    ChatConversation
+        .find({"id":{ '$in': ids}})
+        .populate('participants')
+        .exec(function (err, Competitions) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting Competition.',
+                    error: err
+                });
+            }
+            return res.json(JSON.parse(entities.decode(JSON.stringify(Competitions))));
+        });
+
 };
 
 Service.createChatConversation = function (req, res) {
 
-// use conversation service
+    Object.keys(ChatConversation.schema.obj).forEach(function (key) {
+        object[key] = req.body[key];
+    });
+
+    let conversation = new ChatConversation(object);
+
+    ChatConversation.createChatConversation(conversation, (err, convo) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'Error when creating ChatConversation',
+                error: err
+            });
+        }
+        return res.status(201).json(convo);
+    });
 };
 
 Service.createChatMessage = function (req, res) {
 
-// use chat service
+    // Todo : Is this really necessary or can we just use req.body... ?
+
+    Object.keys(ChatMessage.schema.obj).forEach(function (key) {
+        object[key] = req.body[key];
+    });
+
+    let message = new ChatMessage(object);
+
+    ChatMessage.createChatMessage(message, (err, msg) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'Error when creating ChatMessage',
+                error: err
+            });
+        }
+        return res.status(201).json(msg);
+    });
 };
 
 Service.showChatMessage = function (req, res) {
     // use chat service
+
 };
 
 Service.editChatMessage = function (req, res) {
