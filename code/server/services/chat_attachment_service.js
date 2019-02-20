2368268a636243add0804d910ef8857dc6e0b5cd
ArnaldoLucienTema <arnaldo.trindade@closer>
2019-02-20 23:49:38 +0000

    1 step further on chat
    Former-commit-id: 1092bf60b95b7f406f4fad51c5d7f88d8ccb3990
    Former-commit-id: e2849695f28ad3fbcf74d18c90a7abece8b11b4f

--- code/server/services/chat_attachment_service.js
+++ code/server/services/chat_attachment_service.js
@@ -1,6 +1,68 @@
 let Service = {};
 
-Service.create = function (message) {
-    //
+Service.createChatMessageAttachment = function (user, msgAtt, cb) {
+
+    let chatMessageAttachment = {
+        sender: {
+            name: user.name,
+            info_id: user._id,
+            avatar: user.avatar,
+        },
+        file: {
+            path: msgAtt.file.path,
+            name: msgAtt.file.name,
+            type: msgAtt.file.type,
+            size: msgAtt.file.size
+        },
+        text: msgAtt.text,
+        time_created: Date.now(),
+        chat_conversation_id: msgAtt.chat_conversation_id,
+        deleted: false,
+        archived: false
+    };
+
+    this.save(chatMessageAttachment, function (err, msg) {
+        if (err) {
+            return cb(err)
+        }
+        return cb(null, msg);
+    });
+};
+
+Service.showChatMessageAttachment = function (id) {
+    this.findOne({_id: id}, function (err, messageAttachment) {
+        if (err) {
+            return cb(err);
+        }
+        if (!messageAttachment) {
+            return cb('No such chatMessageAttachment');
+        }
+        return cb(null, messageAttachment);
+    });
+};
+
+Service.editChatMessageAttachment = function (msg, cb) {
+    this.findOneAndUpdate({"_id": msg._id}, msg, {upsert: false, new: true}, cb);
+};
+
+Service.loadMessageAttachmentsByConversationAndUserId = function (conversationId, userId) {
+
+    this.find({
+        chat_conversation_id: conversationId,
+        removed: {"$ne": userId}
+    }, null, {sort: {time_created: -1}}, function (err, chatMessageAttachments) {
+        if (err) {
+            return cb(err);
+        }
+        if (!chatMessageAttachments) {
+            return cb('No messageAttachments for such chatConversationId');
+        }
+        return cb(null, chatMessageAttachments);
+    });
+};
+
+Service.deleteChatMessageAttachment = function (msg, cb) {
+    msg.deleted = true;
+    this.findOneAndUpdate({"_id": msg._id}, msg, {upsert: false, new: true}, cb);
 };
 module.exports = Service;
