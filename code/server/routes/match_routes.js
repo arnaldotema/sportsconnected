5ca1a4fd89851c883d2960e9b19c76b105bf7ec9
ArnaldoLucienTema <arnaldo.trindade@closer>
2019-02-24 12:30:57 +0000

    Task - Endpoint de jogos de equipa personalizado (last X)
    Former-commit-id: 3c8b8c741ab5fcac727cc9437ea4355c1d2d99a4
    Former-commit-id: a21803ad90d8388af5769af49c19b2125080ad6a

--- code/server/routes/match_routes.js
+++ code/server/routes/match_routes.js
@@ -4,6 +4,10 @@ var MatchController = require('../controllers/match_controller.js');
 
 router.get('/', MatchController.list);
 
+router.get('/team/next/:id', MatchController.showNextByTeam);
+
+router.get('/team/last/:id', MatchController.showLastByTeam);
+
 router.get('/:id', MatchController.show);
 
 router.post('/', MatchController.create);
