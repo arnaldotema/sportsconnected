85bb34710d7b72ed8d8e1ed69ef11b19c25dd779
ArnaldoLucienTema <arnaldo.trindade@closer>
2019-02-25 13:46:52 +0000

    CRUD player/media
    Former-commit-id: c6b05985ef394ccc0aa78ae22a58fbb1384ee43e
    Former-commit-id: 8ed520be43dc02c971bfcaab6531c92aaca8ecc3

--- code/server/routes/player_routes.js
+++ code/server/routes/player_routes.js
@@ -2,33 +2,32 @@ var express = require('express');
 var router = express.Router();
 var PlayerController = require('../controllers/user_info_controller.js');
 
-
+router.get('/', PlayerController.list);
 router.post('/', PlayerController.create);
 
-router.post('/:id/recommendations', PlayerController.add_recommendation);
-
-router.post('/:id/skills', PlayerController.add_skill_vote);
-
-router.post('/:id/followers', PlayerController.follow);
+router.get('/:id', PlayerController.show);
+router.put('/:id', PlayerController.update);
+router.delete('/:id', PlayerController.remove);
 
 router.post('/search', PlayerController.search);
 
-router.get('/', PlayerController.list);
-
-router.get('/:id', PlayerController.show);
-
+router.post('/:id/recommendations', PlayerController.add_recommendation);
 router.get('/:id/recommendations', PlayerController.list_recommendations);
 
+router.get('/:id/media', PlayerController.listMedia);
+router.get('/:id/media/:mediaId', PlayerController.showMedia);
+router.post('/:id/media', PlayerController.createMedia);
+router.put('/:id/media/:mediaId', PlayerController.updateMedia);
+router.delete('/:id/media/:mediaId', PlayerController.removeMedia);
+
+router.post('/:id/skills', PlayerController.add_skill_vote);
 router.get('/:id/skills', PlayerController.list_skills);
 
+router.post('/:id/followers', PlayerController.follow);
 router.get('/:id/followers', PlayerController.list_followers);
+router.delete('/:id/followers/:follower_id', PlayerController.unfollow);
 
 router.get('/:id/followed', PlayerController.list_followed);
 
-router.put('/:id', PlayerController.update);
-
-router.delete('/:id', PlayerController.remove);
-
-router.delete('/:id/followers/:follower_id', PlayerController.unfollow);
 
 module.exports = router;
