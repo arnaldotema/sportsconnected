67033cdaa290b47ac2b8ec5c80f1ce16238e794d
ArnaldoLucienTema <pywbal@hotmail.com>
2019-07-03 19:32:58 +0100

    Testing

--- server/config/index.js
+++ server/config/index.js
@@ -11,8 +11,9 @@ module.exports = {
   database:
     process.env.NODE_ENV === "production"
       ? "mongodb://admin:pywball12@199.247.15.139:27017/sports_connected?authSource=admin"
-      : process.env.MONGO_URL,
-  // : "mongodb://mongo:27017/sports_connected",
-  //: "mongodb://localhost:27017/sports_connected",
+      : process.env.MONGO_URL
+      ? process.env.MONGO_URL
+      : "mongodb://mongo:27017/sports_connected",
+  //: 'mongodb://localhost:27017/sports_connected',
   port: 3000
 };
