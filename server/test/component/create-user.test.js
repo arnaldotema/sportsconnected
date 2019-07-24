dbd52de6fc8e1cedd88a3cc708a9669bed4d5f49
ArnaldoLucienTema <pywbal@hotmail.com>
2019-07-24 20:31:38 +0100

    Create component test for updating Users

--- server/test/component/create-user.test.js
+++ server/test/component/create-user.test.js
@@ -1,11 +1,15 @@
 "use strict";
 
+const Chance = require("chance");
 const { assert } = require("chai");
 const { api } = require("../utils");
 const { startServer, stopServer } = require("./../../lib/app");
 const User = require("../../lib/models/football_user");
+const { ObjectID } = require("mongoose").mongo;
 
-describe("Component test: POST /users", () => {
+const chance = new Chance();
+
+describe("Component test: /users", () => {
   before(async () => {
     await startServer();
   });
@@ -41,5 +45,38 @@ describe("Component test: POST /users", () => {
       last_login: actualResponse.last_login,
       subscription_expiration: actualResponse.subscription_expiration
     });
+  });
+
+  it("Should update a user and get it", async () => {
+    const id = chance.string({ length: 24, pool: "1" });
+
+    const user = {
+      _id: id,
+      profile_id: "",
+      user_type: "football_user_info",
+      email: "some@email.com",
+      password: "somepasswordwithmorethan10chars"
+    };
+    const userDocument = new User(user);
+
+    await userDocument.save();
+
+    const { body: actualResponse } = await api
+      .put(`/api/users/${id}`)
+      .set("Content-Type", "application/json")
+      .send({ ...user, email: "someNewemail@email.com" })
+      .expect(200);
+
+    const expectedResponse = { ...user };
+
+    assert.deepEqual(actualResponse, expectedResponse);
+  });
+
+  it("Should get a user by id", async () => {
+    // todo
+  });
+
+  it("Should list users", async () => {
+    // todo
   });
 });
